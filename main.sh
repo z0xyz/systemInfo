ec6b3108c9f661d3d0247a1970bee40ea9c0f588
z0xyz <solicitmind@gmail.com>
2021-10-25 23:03:08 +0200

    new commit with linux kernel info

--- main.sh
+++ main.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
+kernel_name=$(uname -s)
 
-echo "Kernel version"
-uname -r
+echo "Current kernel version"
+uname -mrs
+
+echo ""
+
+if  [ $kernel_name == "Linux" ]
+then
+    echo "Installed kernels"
+    if command -v dnf &> /dev/null
+    then
+        dnf list installed kernel | tail -n +2 | awk '{ print $1 , $2 }'
+    elif command -v dpkg &> /dev/null
+    then
+        dpkg --list | grep linux-image | awk '{ print $2 }'
+    fi
+elif [ $kernel_name == "FreeBSD" ]
+then
+    echo "The kernel type is FreeBsd"
+fi
+
+#if redhat based distro
+rpm -qa kernel
