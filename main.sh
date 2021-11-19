ec9caee260e2fedb992404ca449fa9cfc503c5fc
z0xyz <solicitmind@gmail.com>
2021-11-19 20:50:22 +0200

    Added New battery infromations

--- main.sh
+++ main.sh
@@ -6,18 +6,31 @@ kernel_info(){
     local kernel_name=$(uname -s)
 
     echo "Current kernel version"
-    echo -e "$red_ansi $(uname -mrs)"
+    uname -mrs
 
-    echo ""
+    echo
 
     if  [ $kernel_name == "Linux" ]
     then
         echo 'Installed kernels'
-        echo -e "$red_ansi $(ls -l /lib/modules | awk '{ print $9} ' | tail -n +2)"
+        ls -l /lib/modules | awk '{ print $9} ' | tail -n +2
     elif [ $kernel_name == "FreeBSD" ]
     then
         echo "The kernel type is FreeBsd"
     fi
 }
 
+battery_info(){
+    local current_battery_status='/sys/class/power_supply/BAT0/charge_full'
+    local initial_battery_status='/sys/class/power_supply/BAT0/charge_full_design'
+    local battery_percentage="/sys/class/power_supply/BAT0/capacity"
+    if [ -e $current_battery_status ] && [ -e $initial_battery_status ] && [ -e $battery_percentage ];then
+        local battery_health="$(( $(cat $current_battery_status) / $(cat $initial_battery_status) * 100 ))"
+        echo "Battery Health    $battery_health"
+        echo "Battery percentage  $(cat $battery_percentage)"
+    fi
+
+}
+
 kernel_info
+battery_info
